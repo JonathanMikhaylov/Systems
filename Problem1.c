49b52af0c65ad3c6e3ed4ddcc4ab335c93e1dc85
Jonathan Mikhaylov <JonathanMikhaylov@jonathans-mbp-2.home>
2016-09-20 00:30:25 -0400

    added final file

--- Problem1.c
+++ Problem1.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 int main() {
-  printf( %1problem() );
+  printf("Sum of multiples of 3 and 5: %d",problem() );
   
 }
 
 int problem() {
-  int ret = 0;
+  int sum;
+  sum = 0;
+
   int i;
   for (i=0;i<=100;i++) {
-    if (i%3 == 0) ret+=i;
-    else if (i%5 == 0) ret+=i; 
+    if (i%3 == 0) sum+=i;
+    else if (i%5 == 0) sum+=i; 
   }
-  return ret;
+  return sum;
 }
